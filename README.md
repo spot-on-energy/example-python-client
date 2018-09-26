22506df20ed1cbacbfee445288be94a31b9a68ae
Victor Haverkort <vhaverkort@gmail.com>
2018-09-26 13:42:00 +0200

    Updated documentation

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
-# example-python-client
+# Example python client
 
-This is an example client written in python for accessing the spot-on.energy forecasting service
+This is an example python client for demonstrating how to connect with the spot-on.energy forecasting API.
+
+The forecasting API is secured with [HMAC Signature authentication](https://tools.ietf.org/html/draft-cavage-http-signatures-10).
+The code below demonstrates how to create valid request headers in order to access the data from the API.
+
+## Usage
+
+```python
+import requests
+from auth import generate_request_headers
+import os
+
+# Retrieve credentials from environment variables.
+username = os.getenv('USERNAME')
+secret = os.getenv('SECRET')
+
+url = 'https://api.spot-on.energy/v1/forecasts'
+
+# Set the query parameters. Depending on your subscription, these might be restricted.
+# See https://spot-on.energy/#products.
+query_params = {'markets' : 'EPEX_NL_DAY_AHEAD',
+                'days'    : 1}
+
+# Generate the request headers. The header fields that are used are *Authorization* and *Date*.
+# The hmac signature is included in the *Authorization* field.
+get_request_headers = generate_request_headers(username, secret, url, query_params)
+
+# An example request
+r = requests.get(url, headers=get_request_headers, params=query_params)
+
+# Verify that the response is ok and print the result.
+print('Response code: {}\n'.format(r.status_code))
+print(r.text)
+```  
+
+
--- client.py
+++ client.py
@@ -1,17 +1,32 @@
+# -*- coding: utf-8 -*-
+"""Example python client for connecting with the spot-on.energy forecasting API.
+
+The forecasting API is secured with [HMAC Signature authentication](https://tools.ietf.org/html/draft-cavage-http-signatures-10).
+The code below demonstrates how to create valid request headers in order to access the data from the API.
+"""
+
 import requests
 from auth import generate_request_headers
 import os
 
-key_id = os.getenv('USERNAME', 'test')
-secret = os.getenv('SECRET', 'test')
-url = 'https://dev-kong.spot-on.energy/v1/forecasts'
+# Retrieve credentials from environment variables.
+username = os.getenv('USERNAME')
+secret = os.getenv('SECRET')
 
+url = 'https://api.spot-on.energy/v1/forecasts'
+
+# Set the query parameters. Depending on your subscription, these might be restricted.
+# See https://spot-on.energy/#products.
 query_params = {'markets' : 'EPEX_NL_DAY_AHEAD',
                 'days'    : 1}
 
-get_request_headers = generate_request_headers(key_id, secret, url, query_params)
+# Generate the request headers. The header fields that are used are *Authorization* and *Date*.
+# The hmac signature is included in the *Authorization* field.
+get_request_headers = generate_request_headers(username, secret, url, query_params)
 
-r = requests.get(url, headers=get_request_headers, params=query_params, verify=False)
+# An example request
+r = requests.get(url, headers=get_request_headers, params=query_params)
 
+# Verify that the response is ok and print the result.
 print('Response code: {}\n'.format(r.status_code))
 print(r.text)
